62e536585519be8e819b6077b1eff834c85e86a7
chiakikawataka <113284225+chiakikawataka@users.noreply.github.com>
2025-05-17 15:58:21 +0900

    タイピングゲーム

---
+++ typing_game/script.js
@@ -0,0 +1,38 @@
+// script.js
+
+// 単語のリスト
+const words = ["apple","javascript", "html", "css", "browser", "frontend", "backend", "react", "node", "async", "function"];
+
+let currentWord = "";
+let wordIndex = 0;
+
+const wordDisplay = document.getElementById("word");
+const wordDisplay2 = document.getElementById("word2")
+const input = document.getElementById("input");
+const message = document.getElementById("message");
+const restartBtn = document.getElementById("restart");
+
+// ランダムな単語をセット
+function setNewWord() {
+  currentWord = words[Math.floor(Math.random() * words.length)];
+  wordDisplay.textContent = currentWord;
+  wordDisplay2.textContent = currentWord;
+  input.value = "";
+  message.textContent = "";
+}
+
+// 入力をチェック
+input.addEventListener("input", () => {
+  if (input.value === currentWord) {
+    message.textContent = "正解！次の単語へ";
+    setTimeout(setNewWord, 1000); // 1秒後に次の単語
+  }
+});
+
+// リスタートボタンで最初から
+restartBtn.addEventListener("click", () => {
+  setNewWord();
+});
+
+// 最初の単語を表示
+setNewWord();
